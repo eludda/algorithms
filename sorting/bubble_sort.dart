b41584c260ec34af9573d7ec158759e987fee9bd
eludda <ee@vech.fun>
2026-03-22 21:08:55 +0800

    eludda created a commit ✨

---
+++ sorting/bubble_sort.dart
@@ -0,0 +1,16 @@
+void bubbleSort(List<int> array) {
+  for (int i = 0; i < array.length - 1; i++) {
+    bool swapped = false;
+
+    for (int j = 0; j < array.length - 1 - i; j++) {
+      if (array[j] > array[j + 1]) {
+        int temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+        swapped = true;
+      }
+    }
+
+    if (!swapped) break;
+  }
+}
